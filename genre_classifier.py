9453dbcbeeca2e79bc038ddcc74eea411d961dc1
gilkarin <gilkarin8@gmail.com>
2023-05-18 18:52:54 +0300

    seed

--- genre_classifier.py
+++ genre_classifier.py
@@ -9,7 +9,7 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 
-random.seed(21)
+random.seed(123)
 
 def plot_stft_and_log_mel_spectrogram(audio_data, label, sample_rate=22050, hop_length=512 // 4, n_fft=512, n_mels=80):
     fig, ax = plt.subplots(nrows=1, ncols=1, sharex=True, figsize=(10, 6), gridspec_kw={'hspace': 0.3})
@@ -56,7 +56,7 @@ class TrainingParameters:
     default values (so run won't break when we test this).
     """
     batch_size: int = 32
-    num_epochs: int = 200
+    num_epochs: int = 300
     train_json_path: str = "jsons/train.json"  # you should use this file path to load your train data
     test_json_path: str = "jsons/test.json"  # you should use this file path to load your test data
     # other training hyper parameters
@@ -68,7 +68,7 @@ class OptimizationParameters:
     This dataclass defines optimization related hyper-parameters to be passed to the model.
     feel free to add/change it as you see fit.
     """
-    learning_rate: float = 0.0001
+    learning_rate: float = 0.00008
 
 
 class MusicClassifier:
@@ -197,6 +197,9 @@ class ClassifierHandler:
                 scores = module.forward(feats)
                 module.backward(feats, scores, labels)
         ClassifierHandler.compute_accuracy(test_data, test_labels, module)
+        ClassifierHandler.print_tensor_of_weights_as_list(module.first_class_weights)
+        ClassifierHandler.print_tensor_of_weights_as_list(module.second_class_weights)
+        ClassifierHandler.print_tensor_of_weights_as_list(module.third_class_weights)
         return module
 
     @staticmethod
@@ -238,9 +241,11 @@ class ClassifierHandler:
                 cur_data_torch = torch.tensor([])
                 cur_labels_torch = torch.tensor([])
         train_paths_file.close()
-        print("finished loading training data")
-        # convert each weights tensor to a list than print it as a comma separted list
         return train_loader
+
+    @staticmethod
+    def print_tensor_of_weights_as_list(tensor):
+        print(", ".join([str(x) for x in tensor.tolist()]))
 
     @staticmethod
     def get_pretrained_model() -> MusicClassifier:
@@ -249,18 +254,33 @@ class ClassifierHandler:
         hyper-parameters and return the loaded model
         """
         module = MusicClassifier(OptimizationParameters())
-        module.set_weights(torch.tensor([-0.0057978, 0.09079067, -0.06116471, -0.29027027, -0.09404992, -0.16239388,
-                                         -0.16560023, -0.03633982, 0.13538432, -0.1510415, 0.21880685, -0.09579992,
-                                         -0.0398302, 0.02022951, 0.18496828, 0.00722523, 0.561934, -0.17014208,
-                                         -0.02337782, 0.16052568, -0.00093151, -0.00308735, -0.02399822]),
-                           torch.tensor([0.03940499, -0.04013069, 0.00405579, 0.13892996, -0.0576029, -0.10031118,
-                                         0.14940748, 0.01245282, 0.07959052, 0.0941859, -0.01559121, 0.10083368,
-                                         -0.2745416, -0.20979874, -0.02631551, 0.14107245, 0.13260175, -0.3964954,
-                                         0.05904585, -0.01934657, -0.00569618, -0.02464337, -0.02358416]),
-                           torch.tensor([0.0067467, 0.01117177, 0.07332505, 0.00546377, 0.08037535, 0.23784086,
-                                         0.08857453, -0.01758306, -0.11535291, 0.13901068, 0.08652489, -0.13349035,
-                                         0.15270807, 0.16684859, 0.1583327, -0.29116356, -0.33416405, 0.32580003,
-                                         -0.20485455, -0.02429762, -0.00295077, 0.01629465, 0.12602653]))
+        module.set_weights(torch.tensor([-0.011372833512723446, 0.020494626834988594, -0.06394007802009583, -0.12641745805740356,
+                                         -0.030416306108236313, -0.09144598990678787, -0.036103568971157074,
+                                         -0.03880093991756439, 0.006737630348652601, -0.038998037576675415, 0.010391443967819214,
+                                         -0.022182587534189224, -0.007290207780897617, 0.00145872519351542, 0.01924142614006996,
+                                         0.017280176281929016, 0.047399379312992096, 0.00899857934564352, -0.005552022252231836,
+                                         0.02932639606297016, -0.007758110295981169, 0.015352180227637291,
+                                         0.005761635955423117, 0.021408889442682266, -0.0013940792996436357, 0.019870156422257423,
+                                         -0.002837357809767127, 0.03088158369064331, 0.006957559380680323, -0.005237983539700508,
+                                         -0.00010570632730377838, -0.00036704339436255395, -0.001711581484414637]),
+                           torch.tensor([0.029980603605508804, -0.03357984498143196, -0.028617775067687035, 0.09350784868001938,
+                                         -0.003332694061100483, -0.02301018126308918, 0.02920541912317276, 0.005449625663459301,
+                                         0.008561192080378532, 0.014836831949651241, -0.034509606659412384, 0.01626128889620304,
+                                         -0.05597783252596855, -0.02782018668949604, -0.019905220717191696, -0.008135818876326084,
+                                         0.003259392688050866, -0.0390542633831501, 0.011196278035640717, -0.008717233315110207,
+                                         -0.016262013465166092, -0.018635433167219162, -0.006841812748461962, -0.025590455159544945,
+                                         -0.016384687274694443, -0.041856374591588974, -0.025775324553251266, -0.0010226838057860732,
+                                         0.000834767590276897, -0.0024862566497176886, -0.00010015319276135415, -0.0007900249911472201,
+                                         -0.0007668042671866715]),
+                           torch.tensor([0.0033042789436876774, 0.002013015327975154, 0.04173900932073593, -0.013068262487649918,
+                                         0.04679708927869797, 0.1103283166885376, 0.028105955570936203, 0.018613619729876518,
+                                         -0.00980818085372448, 0.016670173034071922, 0.04528491571545601, -0.03309603035449982,
+                                         0.08379297703504562, 0.011602255515754223, 0.04151991382241249, -0.03373072296380997,
+                                         -0.08778582513332367, 0.04505482688546181, -0.0528569370508194, -0.028757384046912193,
+                                         0.03602603077888489, -0.012033781036734581, -0.03611057996749878, 0.017053689807653427,
+                                         0.008144245482981205, 0.0343603678047657, 0.022465474903583527, -0.02168974280357361,
+                                         -0.028664376586675644, 0.002392763737589121, -0.0002478927781339735, 0.0005581853911280632,
+                                         0.003654906991869211]))
         return module
 
     @staticmethod
